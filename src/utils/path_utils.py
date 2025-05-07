6687a662ba9b7fc9fa0de3cc8d0cb2076ac6ccfa
Oleg <oleg.zendel@rmit.edu.au>
2025-05-07 11:58:23 +1000

    add ensure_dir func

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -33,3 +33,28 @@ def get_data_dir():
     data_dir = os.path.join(get_project_root(), 'data')
     log.debug(f"Data directory path: {data_dir}")
     return data_dir
+
+def ensure_dir(file_path, create_if_not=True):
+    """
+    The function ensures the dir exists,
+    if it doesn't it creates it and returns the path or raises FileNotFoundError
+    In case file_path is an existing file, returns the path of the parent directory
+    """
+    # tilde expansion
+    file_path = os.path.normpath(os.path.expanduser(file_path))
+    if os.path.isfile(file_path):
+        directory = os.path.dirname(file_path)
+    else:
+        directory = file_path
+    if not os.path.exists(directory):
+        if create_if_not:
+            try:
+                os.makedirs(directory)
+            except FileExistsError:
+                # This exception was added for multiprocessing, in case multiple processes try to create the directory
+                pass
+        else:
+            raise FileNotFoundError(
+                f"The directory {directory} doesn't exist, create it or pass create_if_not=True"
+            )
+    return directory
