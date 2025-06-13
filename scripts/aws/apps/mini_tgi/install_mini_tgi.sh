f4f82d2053c78b4afa51c35fe42424057716f0a8
Kun Ran <rankun203@gmail.com>
2025-06-13 23:54:56 +1000

    chore: delete unused files

--- scripts/aws/apps/mini_tgi/install_mini_tgi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to install dependencies for mini-TGI
-set -e
-
-echo "Installing mini-TGI dependencies..."
-python3 -m pip install torch transformers fastapi uvicorn accelerate psutil setproctitle 'jinja2>=3.1.0'
-
-echo "Mini-TGI dependencies installed successfully!"
